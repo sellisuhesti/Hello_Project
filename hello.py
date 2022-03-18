e7f8b96a9c7bbef36233825ec3eb090b4aecb99d
sellisuhesti <Sellisuhesti29@gmail.com>
2022-03-18 13:14:25 +0700

    first commit

---
+++ hello.py
@@ -0,0 +1,9 @@
+#import time 
+print ('ini mengitung volume balok')
+p= float (input("panjang = "))
+l= float (input("Lebar = "))
+t= float (input("Tinggi = "))
+
+v= p*l*t
+
+print ("volume balok = %0.2f" %v)
